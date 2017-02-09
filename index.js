24f51148464a91e4136683f36216018682ae6b67
spkelly <spkelly18@gmail.com>
2017-02-09 16:01:47 -0700

    added light functionality

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+
+// <div id="stopButton" class="button">Stop</div>
+// <div id="slowButton" class="button">Slow</div>
+// <div id="goButton" class="button">Go</div>
+
 (function() {
   'use strict';
 
   // YOUR CODE HERE
+  var stopButton = document.getElementById('stopButton');
+  var slowButton = document.getElementById('slowButton');
+  var goButton = document.getElementById('goButton');
+
+
+
+  console.log(stopButton,slowButton,goButton);
+
+  stopButton.addEventListener('click', function(){
+    var light = document.getElementById('stopLight');
+    light.style.backgroundColor = "red"
+  });
+
+  slowButton.addEventListener('click',function(){
+    var light = document.getElementById('slowLight');
+    light.style.backgroundColor = "yellow";
+  });
+  goButton.addEventListener('click',function(){
+    var light = document.getElementById('goLight');
+    light.style.backgroundColor = "green";
+  });
 })();
