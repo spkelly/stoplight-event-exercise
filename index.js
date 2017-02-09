a3748ec2ed3eb570e086ca5a28e565d00689d29e
spkelly <spkelly18@gmail.com>
2017-02-09 16:40:21 -0700

    simplified light functionality

--- index.js
+++ index.js
@@ -6,26 +6,20 @@
 (function() {
   'use strict';
 
+  function addLight(button, lightID, className){
+    var light = document.getElementById(lightID);
+    button.addEventListener('click',function(){
+      light.classList.toggle(className);
+    });
+  }
+
   // YOUR CODE HERE
   var stopButton = document.getElementById('stopButton');
   var slowButton = document.getElementById('slowButton');
   var goButton = document.getElementById('goButton');
 
+  addLight(stopButton,'stopLight','stop');
+  addLight(slowButton, 'slowLight','slow');
+  addLight(goButton, 'goLight','go');
 
-
-  console.log(stopButton,slowButton,goButton);
-
-  stopButton.addEventListener('click', function(){
-    var light = document.getElementById('stopLight');
-    light.style.backgroundColor = "red"
-  });
-
-  slowButton.addEventListener('click',function(){
-    var light = document.getElementById('slowLight');
-    light.style.backgroundColor = "yellow";
-  });
-  goButton.addEventListener('click',function(){
-    var light = document.getElementById('goLight');
-    light.style.backgroundColor = "green";
-  });
 })();
